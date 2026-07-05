ccf5739fdca617bb4998b486e55e77bfd2736b2b
Zyenith <40903650+FlareZ123@users.noreply.github.com>
2026-07-05 04:24:59 +0000

    fix: wire audited simulator translation unit

---
+++ src/regidrago_sim_fixed.cpp
@@ -0,0 +1,8 @@
+// Split only to keep the GitHub contents API writes reviewable. The compiler sees one translation unit.
+#include "fixed_parts/part001.inc"
+#include "fixed_parts/part002.inc"
+#include "fixed_parts/part003.inc"
+#include "fixed_parts/part004.inc"
+#include "fixed_parts/part005.inc"
+#include "fixed_parts/part006.inc"
+#include "fixed_parts/part007.inc"

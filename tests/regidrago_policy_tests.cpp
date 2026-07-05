cd5576fda88f1f88391c7ad50cc2ca63ef590133
Zyenith <40903650+FlareZ123@users.noreply.github.com>
2026-07-05 22:59:34 +0000

    test: activate Heavy Ball information fixtures

--- tests/regidrago_policy_tests.cpp
+++ tests/regidrago_policy_tests.cpp
@@ -3,5 +3,6 @@
 #include "policy_fixture_v2/part_001.inc"
 #include "policy_fixture_v2/part_002.inc"
 #include "policy_fixture_v2/part_003.inc"
+#include "policy_fixture_v2/part_003h.inc"
 #include "policy_fixture_v2/part_004a.inc"
 #include "policy_fixture_v2/part_004b.inc"

281d77f01564a99bf047cf766aaced7ef821e48e
Zyenith <40903650+FlareZ123@users.noreply.github.com>
2026-07-05 07:09:57 +0000

    test: add exact-state policy fixture executable

---
+++ tests/regidrago_policy_tests.cpp
@@ -0,0 +1,7 @@
+// Split only for reviewable GitHub contents-API commits.
+#include "policy_fixture_v2/part_000.inc"
+#include "policy_fixture_v2/part_001.inc"
+#include "policy_fixture_v2/part_002.inc"
+#include "policy_fixture_v2/part_003.inc"
+#include "policy_fixture_v2/part_004a.inc"
+#include "policy_fixture_v2/part_004b.inc"

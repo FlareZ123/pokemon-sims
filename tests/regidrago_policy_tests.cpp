365168b6b0ede53b8583399d33c6e8a7925a318a
Zyenith <40903650+FlareZ123@users.noreply.github.com>
2026-07-06 03:04:35 +0000

    test: activate full-Bench connector regression

--- tests/regidrago_policy_tests.cpp
+++ tests/regidrago_policy_tests.cpp
@@ -4,5 +4,6 @@
 #include "policy_fixture_v2/part_002.inc"
 #include "policy_fixture_v2/part_003.inc"
 #include "policy_fixture_v2/part_003h.inc"
+#include "policy_fixture_v2/part_003i.inc"
 #include "policy_fixture_v2/part_004a.inc"
 #include "policy_fixture_v2/part_004b.inc"

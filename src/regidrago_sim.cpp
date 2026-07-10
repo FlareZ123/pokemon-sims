9a6e721aef4f74a7a0a5100573afb26253cb4cd6
Zyenith <40903650+FlareZ123@users.noreply.github.com>
2026-07-10 07:01:27 +0000

    Route active search methods through empty-deck guards

--- src/regidrago_sim.cpp
+++ src/regidrago_sim.cpp
@@ -32,32 +32,56 @@
 #include "trace_engine_v2/part_005.inc"
 #undef begin_turn
 #include "trace_engine_v2/part_begin_turn_override.inc"
+#define bench_from_hand bench_from_hand_empty_deck_original
+#define bench_oricorio_if_useful bench_oricorio_if_useful_empty_deck_original
 #define needs_tapu_connector needs_tapu_connector_original
 #define bench_tapu_if_useful bench_tapu_if_useful_original
 #include "trace_engine_v2/part_006.inc"
 #undef bench_tapu_if_useful
 #undef needs_tapu_connector
+#undef bench_oricorio_if_useful
+#undef bench_from_hand
 #include "trace_engine_v2/part_007.inc"
+#define play_mysterious_treasure play_mysterious_treasure_empty_deck_original
 #include "trace_engine_v2/part_008a.inc"
 #include "trace_engine_v2/part_008b.inc"
 #include "trace_engine_v2/part_009a.inc"
+#undef play_mysterious_treasure
+#define play_quick_ball play_quick_ball_empty_deck_original
 #include "trace_engine_v2/part_009b1.inc"
+#undef play_quick_ball
 #define play_ultra_ball play_ultra_ball_original
 #include "trace_engine_v2/part_009b2.inc"
 #define play_evolution_incense play_evolution_incense_original
+#define play_earthen_vessel play_earthen_vessel_empty_deck_original
+#define play_brilliant_blender play_brilliant_blender_empty_deck_original
 #define fss_target_after_search_started fss_target_after_search_started_original
 #define attach_fss attach_fss_original
 #include "trace_engine_v2/part_010.inc"
 #undef attach_fss
 #undef fss_target_after_search_started
+#undef play_brilliant_blender
+#undef play_earthen_vessel
 #undef play_evolution_incense
 #undef play_ultra_ball
+#define bench_tapu_if_useful bench_tapu_if_useful_empty_deck_original
 #include "trace_engine_v2/part_tapu_tate_switch_override.inc"
+#undef bench_tapu_if_useful
+#define play_ultra_ball play_ultra_ball_empty_deck_original
+#define play_evolution_incense play_evolution_incense_empty_deck_original
 #include "trace_engine_v2/part_search_item_overrides.inc"
+#undef play_evolution_incense
+#undef play_ultra_ball
 #include "trace_engine_v2/part_010_fss_override.inc"
 #include "trace_engine_v2/part_010_attach_fss_override.inc"
 #define use_fss use_fss_latias_original
+#define play_crispin play_crispin_empty_deck_original
+#define play_professor_burnet play_professor_burnet_empty_deck_original
+#define play_steven play_steven_empty_deck_original
 #include "trace_engine_v2/part_011.inc"
+#undef play_steven
+#undef play_professor_burnet
+#undef play_crispin
 #define play_arven play_arven_original
 #define play_gladion play_gladion_original
 #include "trace_engine_v2/part_012.inc"
@@ -69,7 +93,9 @@
 #undef use_legacy_star
 #include "trace_engine_v2/part_014a.inc"
 #undef use_fss
+#define use_fss use_fss_empty_deck_original
 #include "trace_engine_v2/part_011_fss_latias_override.inc"
+#undef use_fss
 #undef use_celestial_roar
 #include "trace_engine_v2/part_celestial_roar_override.inc"
 #include "trace_engine_v2/part_013_legacy_star_override.inc"
@@ -77,7 +103,9 @@
 #define play_arven play_arven_powerglass_original
 #include "trace_engine_v2/part_012_override.inc"
 #undef play_arven
+#define play_arven play_arven_empty_deck_original
 #include "trace_engine_v2/part_012_powerglass_override.inc"
+#undef play_arven
 #define choose_supporter choose_supporter_original
 #include "trace_engine_v2/part_014b.inc"
 #undef choose_supporter
@@ -85,6 +113,7 @@
 #include "trace_engine_v2/part_team_yell_vstar_override.inc"
 #undef choose_supporter
 #include "trace_engine_v2/part_turo_oricorio_override.inc"
+#include "trace_engine_v2/part_empty_deck_search_override.inc"
 #include "trace_engine_v2/part_014c.inc"
 #include "trace_engine_v2/part_015.inc"
 #include "trace_engine_v2/part_016.inc"

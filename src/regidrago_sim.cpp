8427bed555e070a1b575a30636704e72afff737c
Zyenith <40903650+FlareZ123@users.noreply.github.com>
2026-07-16 04:37:50 +0000

    Include exact external setup candidate actions

--- src/regidrago_sim.cpp
+++ src/regidrago_sim.cpp
@@ -176,6 +176,7 @@
 #include "trace_engine_v2/part_empty_deck_search_override.inc"
 #undef bench_oricorio_if_useful
 #include "trace_engine_v2/part_oricorio_needed_energy_override.inc"
+#include "trace_engine_v2/part_turbo_external_exact.inc"
 #define run_turn run_turn_original
 #include "trace_engine_v2/part_014c.inc"
 #undef run_turn

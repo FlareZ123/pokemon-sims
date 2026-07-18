8c7644577f8c87374117944cedcf48e82727933d
Zyenith <40903650+FlareZ123@users.noreply.github.com>
2026-07-18 03:08:01 +0000

    Activate route-aware Field Blower on current main

--- src/regidrago_sim.cpp
+++ src/regidrago_sim.cpp
@@ -204,6 +204,7 @@
 #include "trace_engine_v2/part_k0_ultra_ball_target_override.inc"
 #undef bench_oricorio_if_useful
 #include "trace_engine_v2/part_oricorio_needed_energy_override.inc"
+#define play_field_blower play_field_blower_original
 #define run_turn run_turn_original
 #include "trace_engine_v2/part_014c.inc"
 #undef run_turn

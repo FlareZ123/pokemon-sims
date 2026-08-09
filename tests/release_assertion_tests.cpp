8c5be069ed2e46eda30857afc53d3aca1338356e
Zyenith <40903650+FlareZ123@users.noreply.github.com>
2026-08-09 03:48:36 -0400

    Cleanup: name the release assertion diagnostic

--- tests/release_assertion_tests.cpp
+++ tests/release_assertion_tests.cpp
@@ -1,8 +1,12 @@
 #include <cassert>
 
+namespace {
+constexpr char kReleaseAssertionMessage[] = "Release test assertions must remain enabled";
+}
+
 int main() {
   // This intentionally false assertion must terminate every test configuration:
   // https://en.cppreference.com/w/cpp/error/assert
-  assert(false && "Release test assertions must remain enabled");
+  assert(false && kReleaseAssertionMessage);
   return 0;
 }

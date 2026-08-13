e0e7c3160f56cc5c4ec8021f83ebdabb1d9d69ae
Zyenith <40903650+FlareZ123@users.noreply.github.com>
2026-08-13 18:14:08 -0400

    Cleanup: register Forest Seal Stone metadata

--- src/cards/card_registry.hpp
+++ src/cards/card_registry.hpp
@@ -5,15 +5,17 @@
 #include "card_definition.hpp"
 #include "trainers/brilliant_blender.hpp"
 #include "trainers/evolution_incense.hpp"
+#include "trainers/forest_seal_stone.hpp"
 #include "trainers/mysterious_treasure.hpp"
 #include "trainers/professors_letter.hpp"
 #include "trainers/quick_ball.hpp"
 
 namespace sim::cards {
 
-inline constexpr std::array<const CardDefinition*, 5> kRegisteredCardDefinitions{
+inline constexpr std::array<const CardDefinition*, 6> kRegisteredCardDefinitions{
     &BrilliantBlender::definition, // Exact ACE SPEC Item: https://api.pokemontcg.io/v2/cards/sv8-164
     &EvolutionIncense::definition, // Exact print: https://api.pokemontcg.io/v2/cards/swsh1-163
+    &ForestSealStone::definition, // Exact Pokémon Tool: https://api.pokemontcg.io/v2/cards/swsh12-156
     &QuickBall::definition,
     &ProfessorsLetter::definition, // Exact XY 123 metadata: https://api.pokemontcg.io/v2/cards/xy1-123
     &MysteriousTreasure::definition, // Exact SM 113 metadata: https://api.pokemontcg.io/v2/cards/sm6-113
@@ -38,6 +40,10 @@ constexpr bool registered_is_trainer_kind(const Card card,
 
 constexpr bool registered_is_item(const Card card) {
   return registered_is_trainer_kind(card, TrainerKind::Item);
+}
+
+constexpr bool registered_is_tool(const Card card) {
+  return registered_is_trainer_kind(card, TrainerKind::Tool);
 }
 
 }  // namespace sim::cards

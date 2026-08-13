6e99e7aad583f9fc8c7d21b9fcf64126a68f2844
Zyenith <40903650+FlareZ123@users.noreply.github.com>
2026-08-13 16:42:01 -0400

    Cleanup: centralize registered card definitions

--- src/cards/card_registry.hpp
+++ src/cards/card_registry.hpp
@@ -1,20 +1,25 @@
 #pragma once
 
+#include <array>
+
 #include "card_definition.hpp"
+#include "trainers/mysterious_treasure.hpp"
 #include "trainers/professors_letter.hpp"
 #include "trainers/quick_ball.hpp"
 
 namespace sim::cards {
 
+inline constexpr std::array<const CardDefinition*, 3> kRegisteredDefinitions{
+    &QuickBall::definition, // Exact card data: https://api.pokemontcg.io/v2/cards/swsh1-179
+    &ProfessorsLetter::definition, // Exact card data: https://api.pokemontcg.io/v2/cards/xy1-123
+    &MysteriousTreasure::definition, // Exact card data: https://api.pokemontcg.io/v2/cards/sm6-113
+};
+
 constexpr const CardDefinition* find_definition(const Card card) {
-  switch (card) {
-    case Card::QuickBall:
-      return &QuickBall::definition;
-    case Card::ProfessorsLetter:
-      return &ProfessorsLetter::definition; // Exact XY 123 metadata: https://api.pokemontcg.io/v2/cards/xy1-123
-    default:
-      return nullptr;
+  for (const CardDefinition* definition : kRegisteredDefinitions) {
+    if (definition->id == card) return definition;
   }
+  return nullptr;
 }
 
 constexpr bool has_definition(const Card card) {

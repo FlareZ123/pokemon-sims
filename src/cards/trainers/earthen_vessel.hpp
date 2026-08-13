a0d4138689da84939e90910f36b7d79216d524ce
Zyenith <40903650+FlareZ123@users.noreply.github.com>
2026-08-13 16:48:17 -0400

    Cleanup: add Earthen Vessel card definition

---
+++ src/cards/trainers/earthen_vessel.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "../card_definition.hpp"
+
+namespace sim::cards {
+
+class EarthenVessel final {
+ public:
+  static constexpr CardDefinition definition{
+      .id = Card::EarthenVessel,
+      .canonical_id = "sv4-163", // Exact card data: https://api.pokemontcg.io/v2/cards/sv4-163
+      .name = "Earthen Vessel",
+      .kind = CardKind::Trainer,
+      .trainer_kind = TrainerKind::Item, // Printed Item classification: https://api.pokemontcg.io/v2/cards/sv4-163
+      .source_url = "https://api.pokemontcg.io/v2/cards/sv4-163",
+  };
+};
+
+}  // namespace sim::cards

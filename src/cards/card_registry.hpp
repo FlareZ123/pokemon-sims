d4a559a96895954972513cbb4062f7bb627f55cd
Zyenith <40903650+FlareZ123@users.noreply.github.com>
2026-08-13 15:52:45 -0400

    Cleanup: centralize registered metadata lookup

--- src/cards/card_registry.hpp
+++ src/cards/card_registry.hpp
@@ -18,26 +18,13 @@ constexpr const CardDefinition* find_definition(const Card card) {
 }
 
 constexpr bool has_definition(const Card card) {
-  switch (card) {
-    case Card::QuickBall:
-    case Card::ProfessorsLetter: // Printed Item: https://api.pokemontcg.io/v2/cards/xy1-123
-      return true;
-    default:
-      return false;
-  }
+  return find_definition(card) != nullptr;
 }
 
 constexpr bool registered_is_item(const Card card) {
-  switch (card) {
-    case Card::QuickBall:
-      return QuickBall::definition.kind == CardKind::Trainer &&
-             QuickBall::definition.trainer_kind == TrainerKind::Item;
-    case Card::ProfessorsLetter:
-      return ProfessorsLetter::definition.kind == CardKind::Trainer &&
-             ProfessorsLetter::definition.trainer_kind == TrainerKind::Item; // https://api.pokemontcg.io/v2/cards/xy1-123
-    default:
-      return false;
-  }
+  const CardDefinition* definition = find_definition(card);
+  return definition != nullptr && definition->kind == CardKind::Trainer &&
+         definition->trainer_kind == TrainerKind::Item;
 }
 
 }  // namespace sim::cards

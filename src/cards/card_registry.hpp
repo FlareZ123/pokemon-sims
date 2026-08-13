d8bf46980991774285f9bc63d6c701da43d3be68
Zyenith <40903650+FlareZ123@users.noreply.github.com>
2026-08-13 17:16:09 -0400

    Cleanup: register Evolution Incense metadata

--- src/cards/card_registry.hpp
+++ src/cards/card_registry.hpp
@@ -3,12 +3,14 @@
 #include <array>
 
 #include "card_definition.hpp"
+#include "trainers/evolution_incense.hpp"
 #include "trainers/professors_letter.hpp"
 #include "trainers/quick_ball.hpp"
 
 namespace sim::cards {
 
-inline constexpr std::array<const CardDefinition*, 2> kRegisteredCardDefinitions{
+inline constexpr std::array<const CardDefinition*, 3> kRegisteredCardDefinitions{
+    &EvolutionIncense::definition, // Exact print: https://api.pokemontcg.io/v2/cards/swsh1-163
     &QuickBall::definition,
     &ProfessorsLetter::definition, // Exact XY 123 metadata: https://api.pokemontcg.io/v2/cards/xy1-123
 };

8b3a86b5856b3ea4ff8c7211d0c74eb6d9b5200f
Zyenith <40903650+FlareZ123@users.noreply.github.com>
2026-08-14 16:56:53 -0400

    Cleanup: register Erika's Invitation on current main

--- src/cards/card_registry.hpp
+++ src/cards/card_registry.hpp
@@ -15,6 +15,7 @@
 #include "trainers/channeler.hpp"
 #include "trainers/crispin.hpp"
 #include "trainers/dawn.hpp"
+#include "trainers/erikas_invitation.hpp"
 #include "trainers/evolution_incense.hpp"
 #include "trainers/field_blower.hpp"
 #include "trainers/forest_of_vitality.hpp"
@@ -39,7 +40,7 @@
 
 namespace sim::cards {
 
-inline constexpr std::array<const CardDefinition*, 33> kRegisteredCardDefinitions{
+inline constexpr std::array<const CardDefinition*, 34> kRegisteredCardDefinitions{
     &Appletun::definition, // Exact Surging Sparks Stage 1 Dragon: https://api.pokemontcg.io/v2/cards/sv8-140 ; cleanup: https://github.com/FlareZ123/pokemon-sims/issues/3642
     &MawileGX::definition, // Exact Unified Minds Basic Metal Pokémon-GX: https://api.pokemontcg.io/v2/cards/sm11-141 ; cleanup: https://github.com/FlareZ123/pokemon-sims/issues/3704
     &Oricorio::definition, // Exact Guardians Rising Basic Psychic Pokémon: https://api.pokemontcg.io/v2/cards/sm2-55 ; cleanup: https://github.com/FlareZ123/pokemon-sims/issues/3712
@@ -52,6 +53,7 @@ inline constexpr std::array<const CardDefinition*, 33> kRegisteredCardDefinition
     &Channeler::definition, // Exact Unified Minds Supporter: https://api.pokemontcg.io/v2/cards/sm11-190 ; cleanup: https://github.com/FlareZ123/pokemon-sims/issues/3624
     &Crispin::definition, // Exact Stellar Crown Supporter: https://api.pokemontcg.io/v2/cards/sv7-133 ; enhancement: https://github.com/FlareZ123/pokemon-sims/issues/3580
     &Dawn::definition, // Exact Mega Evolution Supporter: https://api.pokemontcg.io/v2/cards/me2-87
+    &ErikasInvitation::definition, // Exact Pokémon 151 Supporter: https://api.pokemontcg.io/v2/cards/sv3pt5-160 ; cleanup: https://github.com/FlareZ123/pokemon-sims/issues/3598
     &EvolutionIncense::definition, // Exact print: https://api.pokemontcg.io/v2/cards/swsh1-163
     &FieldBlower::definition, // Exact SM 125 metadata: https://api.pokemontcg.io/v2/cards/sm2-125
     &ForestOfVitality::definition, // Exact Mega Evolution Stadium: https://api.pokemontcg.io/v2/cards/me1-117 ; cleanup: https://github.com/FlareZ123/pokemon-sims/issues/3639
@@ -143,6 +145,9 @@ static_assert(definition_matches_registration(Crispin::definition, Card::Crispin
                                               "sv7-133", "Crispin")); // Exact Supporter: https://api.pokemontcg.io/v2/cards/sv7-133 ; cleanup: https://github.com/FlareZ123/pokemon-sims/issues/3580
 static_assert(definition_matches_registration(Dawn::definition, Card::Dawn,
                                               "me2-87", "Dawn")); // Exact Supporter: https://api.pokemontcg.io/v2/cards/me2-87
+static_assert(definition_matches_registration(ErikasInvitation::definition,
+                                              Card::ErikasInvitation, "sv3pt5-160",
+                                              "Erika's Invitation")); // Exact Supporter: https://api.pokemontcg.io/v2/cards/sv3pt5-160 ; cleanup: https://github.com/FlareZ123/pokemon-sims/issues/3598
 static_assert(definition_matches_registration(Powerglass::definition, Card::Powerglass,
                                               "sv6pt5-63", "Powerglass")); // Exact Tool: https://api.pokemontcg.io/v2/cards/sv6pt5-63
 static_assert(definition_matches_registration(DoubleDragonEnergy::definition,

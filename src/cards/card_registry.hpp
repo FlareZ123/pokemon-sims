c409133fb430d96ed31d8b447ef1f14dca0b0b79
Zyenith <40903650+FlareZ123@users.noreply.github.com>
2026-08-14 11:51:22 -0400

    Cleanup: register Oricorio card definition

--- src/cards/card_registry.hpp
+++ src/cards/card_registry.hpp
@@ -6,6 +6,7 @@
 #include "energy/double_dragon_energy.hpp"
 #include "pokemon/appletun.hpp"
 #include "pokemon/mawile_gx.hpp"
+#include "pokemon/oricorio.hpp"
 #include "pokemon/regidrago_v.hpp"
 #include "trainers/arven.hpp"
 #include "trainers/battle_vip_pass.hpp"
@@ -34,9 +35,10 @@
 
 namespace sim::cards {
 
-inline constexpr std::array<const CardDefinition*, 28> kRegisteredCardDefinitions{
+inline constexpr std::array<const CardDefinition*, 29> kRegisteredCardDefinitions{
     &Appletun::definition, // Exact Surging Sparks Stage 1 Dragon: https://api.pokemontcg.io/v2/cards/sv8-140 ; cleanup: https://github.com/FlareZ123/pokemon-sims/issues/3642
     &MawileGX::definition, // Exact Unified Minds Basic Metal Pokémon-GX: https://api.pokemontcg.io/v2/cards/sm11-141 ; cleanup: https://github.com/FlareZ123/pokemon-sims/issues/3704
+    &Oricorio::definition, // Exact Guardians Rising Basic Psychic Pokémon: https://api.pokemontcg.io/v2/cards/sm2-55 ; cleanup: https://github.com/FlareZ123/pokemon-sims/issues/3712
     &RegidragoV::definition, // Exact Silver Tempest Basic Dragon Pokémon V: https://api.pokemontcg.io/v2/cards/swsh12-135 ; cleanup: https://github.com/FlareZ123/pokemon-sims/issues/3683
     &DoubleDragonEnergy::definition, // Exact Roaring Skies Special Energy: https://api.pokemontcg.io/v2/cards/xy6-97
     &Arven::definition, // Exact Scarlet & Violet Supporter: https://api.pokemontcg.io/v2/cards/sv1-166
@@ -151,5 +153,7 @@ static_assert(definition_matches_registration(ProfessorTuroScenario::definition,
                                               "Professor Turo's Scenario")); // Exact Supporter: https://api.pokemontcg.io/v2/cards/sv4-171 ; cleanup: https://github.com/FlareZ123/pokemon-sims/issues/3632
 static_assert(definition_matches_registration(MawileGX::definition, Card::MawileGX,
                                               "sm11-141", "Mawile-GX")); // Exact Basic Metal Pokémon-GX: https://api.pokemontcg.io/v2/cards/sm11-141 ; cleanup: https://github.com/FlareZ123/pokemon-sims/issues/3704
+static_assert(definition_matches_registration(Oricorio::definition, Card::Oricorio,
+                                              "sm2-55", "Oricorio GRI 55")); // Exact Basic Psychic Pokémon: https://api.pokemontcg.io/v2/cards/sm2-55 ; compatibility trace: https://github.com/FlareZ123/pokemon-sims/blob/main/tests/issue_2310_turo_oricorio_trace_order_tests.cpp ; cleanup: https://github.com/FlareZ123/pokemon-sims/issues/3712
 
 }  // namespace sim::cards

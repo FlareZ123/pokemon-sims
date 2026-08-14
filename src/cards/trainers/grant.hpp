a9fcbd47ec79d87845cff62fe915aedda2f5a352
Zyenith <40903650+FlareZ123@users.noreply.github.com>
2026-08-14 00:30:03 -0400

    Cleanup: add Grant card definition

---
+++ src/cards/trainers/grant.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "../card_definition.hpp"
+
+namespace sim::cards {
+
+class Grant final {
+ public:
+  static constexpr CardDefinition definition{
+      .id = Card::Grant,
+      .canonical_id = "swsh10-144", // Exact Astral Radiance print: https://api.pokemontcg.io/v2/cards/swsh10-144
+      .name = "Grant",
+      .kind = CardKind::Trainer, // Trainer/Supporter procedure: https://github.com/FlareZ123/pokemon-sims/blob/main/EN_advanced_manual-2025-transcription-structured.md
+      .trainer_kind = TrainerKind::Supporter, // Exact subtype and one-Supporter reminder: https://api.pokemontcg.io/v2/cards/swsh10-144
+      .source_url = "https://api.pokemontcg.io/v2/cards/swsh10-144", // Cleanup issue: https://github.com/FlareZ123/pokemon-sims/issues/3589
+  };
+};
+
+}  // namespace sim::cards

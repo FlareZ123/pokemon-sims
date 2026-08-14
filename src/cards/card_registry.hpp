e0ab7b81940ebc1f8ce25a7ef8305af533c0478b
Zyenith <40903650+FlareZ123@users.noreply.github.com>
2026-08-14 05:04:11 -0400

    Cleanup: centralize registered trainer subtype helpers

--- src/cards/card_registry.hpp
+++ src/cards/card_registry.hpp
@@ -78,6 +78,14 @@ constexpr bool registered_is_supporter(const Card card) {
   return registered_is_trainer_kind(card, TrainerKind::Supporter);
 }
 
+constexpr bool registered_is_tool(const Card card) {
+  return registered_is_trainer_kind(card, TrainerKind::Tool);
+}
+
+constexpr bool registered_is_stadium(const Card card) {
+  return registered_is_trainer_kind(card, TrainerKind::Stadium);
+}
+
 constexpr bool registered_is_ace_spec(const Card card) {
   const CardDefinition* definition = find_definition(card);
   return definition != nullptr && definition->ace_spec;
